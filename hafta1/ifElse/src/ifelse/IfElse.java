8327a62ab74111743db8dd3cc1c2c6cbaa9ceebd
oznurozen <oznurozen67@gmail.com>
2021-09-29 12:33:46 +0300

    birinci hafta kodlari

---
+++ hafta1/ifElse/src/ifelse/IfElse.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelse;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class IfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         /* int sayi1 = 50;
+	       int sayi2 = 50;
+	       if(sayi1 == sayi2){
+	           System.out.println("Bu iki sayı birbirine eşittir.");
+	       }
+	       else{
+	            System.out.println("Bu iki sayı birbirine eşit değildir.");
+	       }*/
+     
+       /* int s1 =10;
+        int s2 =20;
+        if(s1>s2){
+            System.out.println(s1);
+        }
+        else{
+            System.out.println(s2);
+        }
+        */
+      
+      //100-90 arası AA
+     // 89-70 arası BB
+     //69-50 arası CC
+     //49-30 arası DD
+     // 30< FF
+     
+    /* int not =25;
+     
+     if(not<=100 && not>=90){
+         System.out.println("AA");
+     }
+     else if(not<=89 && not>=70){
+         System.out.println("BB");
+     }
+     else if(not<=69 && not>=50){
+         System.out.println("CC");
+     }
+     else if(not<=49 && not>=30){
+         System.out.println("DD");
+     }
+     else{
+         System.out.println("FF");
+     }*/
+    /*
+    int ogrenci1 =80 , ogrenci2 = 90, ogrenci3 = 100;
+    if(ogrenci1>ogrenci2 && ogrenci1>ogrenci3){
+        System.out.println("En yüksek not: " + ogrenci1);
+    }
+    else if(ogrenci2>ogrenci1 && ogrenci2>ogrenci3){
+        System.out.println("En yüksek not: " + ogrenci2);
+    }
+    else{
+        System.out.println("En yüksek not: " + ogrenci3);
+    }
+
+      */
+    
+    /*
+   5 sayı değişkeni oluşturun, içlerine değerlerini atayın ve bu sayıları
+    7 sayısı ile çarpma işlemine tabii tutun çarpma işleminin ardından 
+    tek olan sayıları ekrana yazdırmaya çalışın.
+
+    
+    int sayi1=1;
+        int sayi2=2;
+        int sayi3=3;
+        int sayi4=4;
+        int sayi5=5;
+        int s1=sayi1*7;
+        int s2=sayi2*7;
+        int s3=sayi3*7;
+        int s4=sayi4*7;
+        int s5=sayi5*7;
+        if (s1%2==1){
+        System.out.println(sayi1);
+    }
+        if (s2%2==1){
+            System.out.println(sayi2);
+        }
+        if (s3%2==1){
+            System.out.println(sayi3);
+        }
+        if (s4%2==1){
+            System.out.println(sayi4);
+        }
+        if (s5%2==1){
+            System.out.println(sayi5);
+        }
+        else {
+            System.out.println("Hicbiri tek degildir");
+        }
+
+    3 adet sayı tanımlayınız ve birbirleri arasındaki büyüklük ve küçüklük durumunu gösteriniz.
+
+       
+    
+        int sayi1=90, sayi2 = 15, sayi3 = 100;
+        if(sayi1>sayi2){
+            System.out.println("sayi1, sayi2 den büyüktür.");
+            if(sayi1>sayi3){
+                System.out.println("sayi1, sayi3 den büyüktür."); 
+            }
+            else{
+                System.out.println("sayi1, sayi3 den büyük değildir."); 
+            }
+            
+        }
+        else{
+            System.out.println("sayi1, sayi2 den büyük değildir."); 
+        }*/
+    
+    /*Sürücü hızını içeren bir adet değişken tanımlayınız. Eğer sürücünün hızı 90 dan büyük ise radara 
+    girdiniz bloğu çalışsın. Hız 90 dan büyük ve eşit, 120 den küçük ise cezanız 200 Tl olarak ekrana 
+    yazsın. Eğer hız 120 den büyük ve eşit ise cezanız 400 tl olarak ekrana versin.
+    Sürücünün hızı 90 dan küçük olması durumunda ise ekrana hızınız normal yazısını versin.
+    
+       
+     int hiz =50;
+     if(hiz>90){
+         System.out.println("Radara girdiniz.");
+         if(hiz<120){
+             System.out.println("Cezanız 200 TL.");
+         }
+         else if(hiz>=120){
+             System.out.println("Cezanız 400 TL.");
+         }
+     }
+     else{
+         System.out.println("Hızınız normal");
+     }*/
+    
+    /*Bir değişken içerisinde 5 adet sayı değeri atayınız. Ardından bu sayıların çift mi 
+    yoksa tek sayı mı olduğunu kontrol ederek çift olanları ekrana yazdırınız.
+    
+
+     int s1=7, s2=40, s3=11, s4=51, s5=17;
+     if(s1%2==0){
+         System.out.println(s1);
+     }
+     if(s2%2==0){
+         System.out.println(s2);
+     }
+     if(s3%2==0){
+         System.out.println(s3);
+     }
+     if(s4%2==0){
+         System.out.println(s4);
+     }
+     if(s5%2==0){
+         System.out.println(s5);
+     }
+     else{
+         System.out.println("Sayıların hepsi tek sayıdır.");
+     }
+*/
+    /*Bir değişken içerisinde sayı değeri atayınız. Ardından bu sayının
+    çift mi yoksa tek sayı mı olduğunu ekrana yazdırınız.
+
+    int degisken = 4;
+    if(degisken%2==0){
+        System.out.println(degisken + " Sayısı çift sayıdır.");
+    }
+    else{
+         System.out.println(degisken + " Sayısı tek sayıdır.");
+    }
+*/
+     
+    }
+    
+}
---
+++ hafta1/variables/src/variables/Variables.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /*  int sayi = 35;
+        String metin = "Sayı : ";
+       System.out.println(metin + sayi);
+       System.out.println(metin + sayi);
+       System.out.println(metin + sayi);
+       System.out.println(metin + sayi);
+       System.out.println(metin + sayi);
+       
+       int sayiDegeri = 5;
+       System.out.println(sayiDegeri);
+       
+       double doubleSayi = 10.5;
+       System.out.println(doubleSayi);
+       
+       float floatSayi = (float)20.5;
+       
+       char charTip = 'a';
+       
+       int sayi1 = 5;
+       int sayi2 = 5;
+       boolean sonuc = (sayi1 == sayi2);
+       System.out.println(sonuc);
+       
+       byte byteSayi = -129;
+       
+       var varDegisken =5;
+      
+     int sayi = 5;
+     sayi=6;
+     System.out.println(sayi);
+     
+     int a =5;
+     int b= 10;
+     a = a-b;
+     b=b+2;
+     System.out.println(a +" "+ b);
+      
+      int a = (3+5)/2;
+      System.out.println(a);
+      
+      int b = 19%5;
+      System.out.println(b);
+      
+      int c =15;
+      int d =20;
+      c += d ;
+      System.out.println(c);
+      
+      int sayi1 = 9;
+      sayi1++;
+      System.out.println(sayi1);
+      int sayi1 = 8;
+      double sayi2 = 1.80;
+      double modAlma = sayi1%sayi2;
+      System.out.println(modAlma);
+      System.out.println(" " + modAlma+sayi2);
+        
+        
+       Sonradan değiştirilmesi mümkün olmayacak şekilde bir 
+        string ifade tanımlayınız ve bu değişken içerisine kendi
+        isminizi atayınız. Ardından sayısal veri tipinde 2 adet
+        2 basamaklı sayı tanımlayınız ve bu iki sayıyı toplayıp 3 ile 
+        çarptıktan sonra 2 ye bölün. Tipini belirleyemediğimiz bir değişken 
+        türü daha oluşturduktan sonra içerisine istediğiniz bir değeri atayarak 
+        tüm bu adımları ekranda yan yana gelecek ve aralarında 1 adet boşluk olacak şekilde yazdırın.
+
+        
+      
+      final String isim = " Oznur";
+      int sayi1 = 20;
+      int sayi2 = 30;
+      int sonuc =(sayi1+sayi2)*3/2;
+      var degisken = 5;
+      System.out.println(isim + " " + sonuc + " " + degisken);
+        
+        
+        Basit bir öğrenci kaldı geçti işlemi gerçekleştirin. Bir değişken içerisine 
+        2 adet not değeri atayın ve bu iki notun ortalamasını alın. Boolean tipini 
+        kullanarak girilen değerlerin ortalamasının 50 den büyük olması durumunda geçti 
+        yani true ifadesi ekrana yazılsın, notun ortalamasının 50 den küçük olması durumunda
+        kaldı anlamı taşıyan false ifadesi ekrana gelsin.
+
+      
+      int vize =10;
+      int finalNotu = 10;
+      int ortalama = (vize+ finalNotu)/2;
+      boolean sonuc = ortalama >= 50;
+      System.out.println(sonuc);
+      */
+      
+      /*3 adet sayı tanımlayınız ve ekrana çıktı olarak
+        Bu sayıların toplamı: 3 sayının toplamı sonucunu yazdırın.
+      
+     
+        
+      int sayibir = 30, sayiiki = 70, sayiuc =80, toplam = sayibir+ sayiiki+ sayiuc;
+      System.out.println("Bu sayıların toplamı: " + toplam);
+       */
+      
+      /*İnt sayi1 = 80; int sayi2=25 şeklinde iki değişken ve değer ataması sağlayın ardından bu verilerin 
+      birbirleriyle eşit olup olmama durumunu karşılaştırın ve 4 işlem değişkenleri oluşturarak 4 işlem
+      gerçekleştirin ekrana çıktısını veriniz.
+       
+      
+      int sayi1=25, sayi2 =25;
+      boolean esitMi = (sayi1==sayi2);
+      int toplama = sayi1+sayi2;
+      int cikarma = sayi1-sayi2;
+      int carpma = sayi1*sayi2;
+      int bolme = sayi1/sayi2;
+      System.out.println(esitMi);
+      System.out.println("Toplama: "+ toplama + " Çıkarma : "+ cikarma+ " Çarpma : " + carpma + " Bölme : " + bolme);*/
+      
+     /* int deger1 =20, deger2 = 5;
+      int topla =(deger1+deger2);
+      int cikar =(deger1-deger2);
+      int carp=(deger1*deger2);
+      int bol =(deger1/deger2);
+      System.out.println(topla +" "+ cikar+" " + carp + " "+ bol);
+     
+      int sayi1 = 50;
+	       int sayi2 = 50;
+	       if(sayi1 == sayi2){
+	           System.out.println("Bu iki sayı birbirine eşittir.");
+	       }
+	       else{
+	            System.out.println("Bu iki sayı birbirine eşit değildir.");
+	       }*/
+     
+       /* int s1 =10;
+        int s2 =20;
+        if(s1>s2){
+            System.out.println(s1);
+        }
+        else{
+            System.out.println(s2);
+        }
+        */
+      
+      //100-90 arası AA
+     // 89-70 arası BB
+     //69-50 arası CC
+     //49-30 arası DD
+     // 30< FF
+     
+    /* int not =25;
+     
+     if(not<=100 && not>=90){
+         System.out.println("AA");
+     }
+     else if(not<=89 && not>=70){
+         System.out.println("BB");
+     }
+     else if(not<=69 && not>=50){
+         System.out.println("CC");
+     }
+     else if(not<=49 && not>=30){
+         System.out.println("DD");
+     }
+     else{
+         System.out.println("FF");
+     }*/
+    /*
+    int ogrenci1 =80 , ogrenci2 = 90, ogrenci3 = 100;
+    if(ogrenci1>ogrenci2 && ogrenci1>ogrenci3){
+        System.out.println("En yüksek not: " + ogrenci1);
+    }
+    else if(ogrenci2>ogrenci1 && ogrenci2>ogrenci3){
+        System.out.println("En yüksek not: " + ogrenci2);
+    }
+    else{
+        System.out.println("En yüksek not: " + ogrenci3);
+    }
+
+      */
+    
+    /*
+   5 sayı değişkeni oluşturun, içlerine değerlerini atayın ve bu sayıları
+    7 sayısı ile çarpma işlemine tabii tutun çarpma işleminin ardından 
+    tek olan sayıları ekrana yazdırmaya çalışın.
+
+    
+    int sayi1=1;
+        int sayi2=2;
+        int sayi3=3;
+        int sayi4=4;
+        int sayi5=5;
+        int s1=sayi1*7;
+        int s2=sayi2*7;
+        int s3=sayi3*7;
+        int s4=sayi4*7;
+        int s5=sayi5*7;
+        if (s1%2==1){
+        System.out.println(sayi1);
+    }
+        if (s2%2==1){
+            System.out.println(sayi2);
+        }
+        if (s3%2==1){
+            System.out.println(sayi3);
+        }
+        if (s4%2==1){
+            System.out.println(sayi4);
+        }
+        if (s5%2==1){
+            System.out.println(sayi5);
+        }
+        else {
+            System.out.println("Hicbiri tek degildir");
+        }
+
+    3 adet sayı tanımlayınız ve birbirleri arasındaki büyüklük ve küçüklük durumunu gösteriniz.
+
+       
+    
+        int sayi1=90, sayi2 = 15, sayi3 = 100;
+        if(sayi1>sayi2){
+            System.out.println("sayi1, sayi2 den büyüktür.");
+            if(sayi1>sayi3){
+                System.out.println("sayi1, sayi3 den büyüktür."); 
+            }
+            else{
+                System.out.println("sayi1, sayi3 den büyük değildir."); 
+            }
+            
+        }
+        else{
+            System.out.println("sayi1, sayi2 den büyük değildir."); 
+        }*/
+    
+    /*Sürücü hızını içeren bir adet değişken tanımlayınız. Eğer sürücünün hızı 90 dan büyük ise radara 
+    girdiniz bloğu çalışsın. Hız 90 dan büyük ve eşit, 120 den küçük ise cezanız 200 Tl olarak ekrana 
+    yazsın. Eğer hız 120 den büyük ve eşit ise cezanız 400 tl olarak ekrana versin.
+    Sürücünün hızı 90 dan küçük olması durumunda ise ekrana hızınız normal yazısını versin.
+    */
+       
+     int hiz =50;
+     if(hiz>90){
+         System.out.println("Radara girdiniz.");
+         if(hiz<120){
+             System.out.println("Cezanız 200 TL.");
+         }
+         else if(hiz>=120){
+             System.out.println("Cezanız 400 TL.");
+         }
+     }
+     else{
+         System.out.println("Hızınız normal");
+     }
+     
+    }
+    
+}
